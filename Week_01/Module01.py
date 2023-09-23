7f655a300afe2eb1fd29f371cb0b98fcce6ef902
MZahidKamal <kamal.md.zahid@gmail.com>
2023-09-23 23:32:05 +0200

    Daily Learning & Commit

---
+++ Week_01/Module01.py
@@ -0,0 +1,7 @@
+# History of Python.
+# https://www.geeksforgeeks.org/history-of-python/
+#-----------------------------------------------------------------------------------------------------------------------
+print("New Journey: Learning Python")
+print(123)
+print(456)
+print(789)
