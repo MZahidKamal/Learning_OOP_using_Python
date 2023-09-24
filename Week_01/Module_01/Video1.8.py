11c734e7015ea731e23706aa6955835a5994c8be
MZahidKamal <kamal.md.zahid@gmail.com>
2023-09-24 17:07:01 +0200

    Daily Learning & Commit

---
+++ Week_01/Module_01/Video1.8.py
@@ -0,0 +1,33 @@
+#-----------------------------------------------------------------------------------------------------------------------
+# 1.8 for loop in python and range in python
+#-----------------------------------------------------------------------------------------------------------------------
+# Declaring an array and printing the array using for loop.
+# numbers = [5, 10, 15, 20, 25, 30]
+# for x in numbers:
+#     print(x)
+#-----------------------------------------------------------------------------------------------------------------------
+# Declaring an array and printing the array using for loop. In the meantime, counting the sum of the array elements.
+# numbers = [5, 10, 15, 20, 25, 30]
+# summ = 0
+# for x in numbers:
+#     print(x)
+#     summ = summ + x
+# print(summ)
+#-----------------------------------------------------------------------------------------------------------------------
+# Declaring a string and printing the string using for loop.
+# text = "sunday"
+# for char in text:
+#     print(char)
+#-----------------------------------------------------------------------------------------------------------------------
+# Range based for loop in Python. Starting from 0 and ending before 5.
+for i in range(5):
+    print(i)
+
+# Range based for loop in Python. Starting from 1 and ending before 10.
+for i in range(1, 10):
+    print(i)
+
+# Range based for loop in Python. Starting from 1, ending before 10, incrementing 2.
+for i in range(1, 10, 2):
+    print(i)
+#-----------------------------------------------------------------------------------------------------------------------
