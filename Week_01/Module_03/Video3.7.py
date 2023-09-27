4792cde87a44c4cb4b51d47241c3b34ca5825598
MZahidKamal <kamal.md.zahid@gmail.com>
2023-09-27 13:56:04 +0200

    Daily Learning & Commit

--- Week_01/Module_03/Video3.7.py
+++ Week_01/Module_03/Video3.7.py
@@ -1,3 +1,80 @@
-#-----------------------------------------------------------------------------------------------------------------------
+# -----------------------------------------------------------------------------------------------------------------------
 # 3.7 Try except finally and file read write in python
+# -----------------------------------------------------------------------------------------------------------------------
+# 8.3. Handling Exceptions
+# 8.7. Defining Clean-up Actions
+# https://docs.python.org/3/tutorial/errors.html
+# -----------------------------------------------------------------------------------------------------------------------
+
+while True:
+    try:
+        x = int(input("Please enter a number: "))
+        break
+    except ValueError:
+        print("Oops!  That was no valid number.  Try again...")
+
+# Try the above code with 9 and -9.
+# -----------------------------------------------------------------------------------------------------------------------
+# The try statement works as follows:
+#    1. First, the statement(s) between the try and except keywords is executed.
+#    2. If no error/exception occurs, the except clause is skipped and execution of the try statement is finished.
+#    3. If an error/exception occurs during execution of the try clause, the rest of the clause is skipped.
+#    4. Then, the except clause will be executed.
+#    5. A try statement may have more than one except clause.
+# -----------------------------------------------------------------------------------------------------------------------
+# This is another version.
+
+def bool_return():
+    try:
+        return True
+    finally:
+        return False
+
+
+# -----------------------------------------------------------------------------------------------------------------------
+# This is another complicated version.
+
+def divide(x, y):
+    try:
+        result = x / y
+    except ZeroDivisionError:
+        print("division by zero!")
+    else:
+        print("result is", result)
+    finally:
+        print("executing finally clause")
+
+
+# -----------------------------------------------------------------------------------------------------------------------
+# result = 45/0       # Which will produce obvious error. So let's see, how we can handle the exception here.
+
+try:
+    result = 45 / 5
+except:
+    print('Infinite result. Exception handled.')
+finally:
+    print('Finally execution completed.')
+
+print('Done')
+
+# Try the above code with 0 and 5.
+#-----------------------------------------------------------------------------------------------------------------------
+# File handling in Python
+# .csv file => Comma Separated Value
+# .txt file => Text file
+
+# Creating a text file and writing in it.
+# with open('TextFile.txt', 'w') as file:
+#     file.write('I Love You Python !!\n')
+
+# Appending the same text file.
+# with open('TextFile.txt', 'a') as file:
+#     file.write('I Love You Python !!\n')
+
+# Reading the same text file.
+with open('TextFile.txt', 'r') as file:
+    text = file.read()
+    print(text)
+
+# To Learn more, visit (https://docs.python.org/3/library/filesys.html).
 #-----------------------------------------------------------------------------------------------------------------------
--- Week_01/Module_03/Video3.8.py
+++ Week_01/Module_03/Video3.8.py
@@ -1,3 +1,30 @@
 #-----------------------------------------------------------------------------------------------------------------------
 # 3.8 (advanced) lamda function in python
 #-----------------------------------------------------------------------------------------------------------------------
+# Let's create a function.
+
+def double_it(x):
+    return x * 2
+
+result = double_it(25)
+print(result)
+
+#-----------------------------------------------------------------------------------------------------------------------
+# In Python the lambda keyword is used to create anonymous functions.
+# An anonymous function means that a function is without a name.
+# Syntax
+#       lambda arguments : expression
+# This function can have any number of arguments but only one expression, which is evaluated and returned.
+
+# Let's create the same function again, but this time using Lambda keyword.
+
+doubled = lambda x: x * 2
+squared = lambda x: x ** 2
+print(doubled(44))
+print(squared(9))
+#-----------------------------------------------------------------------------------------------------------------------
+# Let's write another function using Lambda, but this time using multiple arguments.
+
+summ = lambda x, y, z : x + y + z
+print(summ(10, 20, 30))
+#-----------------------------------------------------------------------------------------------------------------------
