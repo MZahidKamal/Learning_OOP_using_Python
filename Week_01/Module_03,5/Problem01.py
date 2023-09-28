1bcba4f9c0fef627fe42fb3267ea5b699183ad99
MZahidKamal <kamal.md.zahid@gmail.com>
2023-09-28 02:14:39 +0200

    Daily Learning & Commit

--- Week_01/Module_03,5/Problem01.py
+++ Week_01/Module_03,5/Problem01.py
@@ -2,3 +2,18 @@
 # G. Palindrome Array
 # https://codeforces.com/group/MWSDmqGsZm/contest/219774/problem/G
 #-----------------------------------------------------------------------------------------------------------------------
+#Taking N input.
+N = int(input())
+
+#Creating a list, and then taking inputs into it.
+list_A = list(map(int, input().split()))
+
+#Create a copy of the main list, and then reverse it.
+list_R = list_A.copy()
+list_R.reverse()
+
+#Now doing the comparison and printing the final output.
+if list_A == list_R:
+    print('YES')
+else:
+    print('NO')
--- Week_01/Module_03,5/Problem02.py
+++ Week_01/Module_03,5/Problem02.py
@@ -2,3 +2,16 @@
 # K. Sum Digits
 # https://codeforces.com/group/MWSDmqGsZm/contest/219774/problem/K
 #-----------------------------------------------------------------------------------------------------------------------
+#Taking N input.
+N = int(input())
+
+#Creating a string variable, and then taking input into it.
+input_string = input()
+
+#Now extracting each character, converting them into int, and the calculating the summ variable.
+summ = 0
+for c in input_string:
+    summ = summ + int(c)
+
+#Printing the final output.
+print(summ)
