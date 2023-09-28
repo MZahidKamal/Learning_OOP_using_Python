c3108d9323816513f520d876dddbd8aa96dc549d
MZahidKamal <kamal.md.zahid@gmail.com>
2023-09-28 18:54:04 +0200

    Daily Learning & Commit

--- Week_01/Module_03,5/Problem05.py
+++ Week_01/Module_03,5/Problem05.py
@@ -2,3 +2,16 @@
 # Q. Reverse Words
 # https://codeforces.com/group/MWSDmqGsZm/contest/219856/problem/Q
 #-----------------------------------------------------------------------------------------------------------------------
+#Taking full sentence directly as a string and saved in a variable.
+sentence = list(map(str, input().split()))
+
+#Extracting each word of the sentence, reversing it, and then printing it.
+reversed_sentence_list = []
+for word in sentence:
+    reversed_sentence_list.append(word[::-1])
+
+#Now we'll take all words from the list and will join them using 'space'. To avoid the tailing space.
+reversed_sentence = ' '.join(reversed_sentence_list)
+
+#Printing the final modified sentence.
+print(reversed_sentence)
--- Week_01/Module_03,5/Problem06.py
+++ Week_01/Module_03,5/Problem06.py
@@ -2,3 +2,11 @@
 # V. Replace Word
 # https://codeforces.com/group/MWSDmqGsZm/contest/219856/problem/V
 #-----------------------------------------------------------------------------------------------------------------------
+#Taking the whole string directly as input.
+originalString = input()
+
+#Replacing the target word "EGYPT" with a space.
+modifiedString = originalString.replace("EGYPT", " ")
+
+#Printing the modified string.
+print(modifiedString)
