487ba1a7d4d4535023d29b1f242f84428f76f1ef
MZahidKamal <kamal.md.zahid@gmail.com>
2023-09-29 01:56:53 +0200

    Daily Learning & Commit

--- Week_01/Module_04/Problem01.py
+++ Week_01/Module_04/Problem01.py
@@ -2,3 +2,28 @@
 # S. Max Split
 # https://codeforces.com/group/MWSDmqGsZm/contest/219856/problem/S
 #-----------------------------------------------------------------------------------------------------------------------
+#Taking the balanced string directly.
+InputString = input()
+
+#Creating necessary variable, list and a counter.
+BalancedString = ""
+BalancedStringList = []
+BalancedStringCounter = 0
+
+#Extracting each char from the InputString, adding it into a BalancedString and check if it's balanced or not.
+#If balanced, saving the BalancedString into a list and increase the counter.
+for char in InputString:
+    BalancedString += char
+
+    countL = BalancedString.count('L')
+    countR = BalancedString.count('R')
+
+    if countL == countR:
+        BalancedStringCounter += 1
+        BalancedStringList.append(BalancedString)
+        BalancedString = ""
+
+#Printing the final output.
+print(BalancedStringCounter)
+for word in BalancedStringList:
+    print(word)
