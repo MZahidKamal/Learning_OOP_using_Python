22ab05a36b9b806dc261e6a99e4f880c547c4828
MZahidKamal <kamal.md.zahid@gmail.com>
2023-09-28 08:51:29 +0200

    Daily Learning & Commit

--- Week_01/Module_03,5/Problem03.py
+++ Week_01/Module_03,5/Problem03.py
@@ -2,3 +2,29 @@
 # M. Replace MinMax
 # https://codeforces.com/group/MWSDmqGsZm/contest/219774/problem/M
 #-----------------------------------------------------------------------------------------------------------------------
+#Taking N input.
+N = int(input())
+
+#Creating a list, and then taking inputs into it.
+list_A = list(map(int, input().split()))
+
+#Saving max and min elements of the list.
+max_value = max(list_A)
+min_value = min(list_A)
+
+#Finding the index of max and min from the liat.
+max_index = -1
+min_index = -1
+
+for i in range(N):
+    if list_A[i] == max_value:
+        max_index = i
+    if list_A[i] == min_value:
+        min_index = i
+
+#Swaping the max and min elements.
+list_A[max_index], list_A[min_index] = list_A[min_index], list_A[max_index]
+
+#Printing the modified list.
+for x in list_A:
+    print(x, end=" ")
--- Week_01/Module_03,5/Problem04.py
+++ Week_01/Module_03,5/Problem04.py
@@ -2,3 +2,22 @@
 # Y. Range sum query
 # https://codeforces.com/group/MWSDmqGsZm/contest/219774/problem/Y
 #-----------------------------------------------------------------------------------------------------------------------
+#Taking input of N and the number of test queries.
+N, testQueries = list(map(int, input().split()))
+
+input_List = list(map(int, input().split()))
+
+#Loop iteration for all test cases.
+for test in range(testQueries):
+    # Taking input of left index and right index.
+    L, R = list(map(int, input().split()))
+
+    #Calculating the sum of all elements from left index to right index.
+    summLR = 0
+    for num in input_List[L - 1: R]:
+        summLR = summLR + num
+
+    #Printing the final summ.
+    print(summLR)
+
+# TLE TLE TLE TLE TLE TLE TLE TLE TLE TLE TLE TLE TLE TLE TLE TLE TLE TLE TLE TLE TLE TLE TLE TLE TLE TLE TLE TLE TLE
