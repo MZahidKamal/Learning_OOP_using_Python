67dd7cdffbdf39840c320b67665ea3b71d72a262
MZahidKamal <kamal.md.zahid@gmail.com>
2023-09-29 18:46:05 +0200

    Daily Learning & Commit

--- Week_01/Module_04/Problem02.py
+++ Week_01/Module_04/Problem02.py
@@ -2,37 +2,43 @@
 # C - Good Sequence
 # https://atcoder.jp/contests/arc087/tasks/arc087_a
 #-----------------------------------------------------------------------------------------------------------------------
+# This time try to solve the code using dictionary.
+
+
+
+
+
+"""
 #Taking the input N as the size of the list. And then taking the list input.
 N = int(input())
 ListA = list(map(int, input().split()))
 
 #Creating a counter to count how many we'll have to remove.
 removalCounter = 0
+alreadyChecked = []
 
-#Loop iteration to extract each num, check how many of this num we have, saving the count and replacing all num with -1.
-#If the numCount and num are equal then ok.
+#Loop iteration to extract each num, check how many of this num we have, saving the count.
+#Saving the num in a list called alreadyChecked.
 #If the numCount is more than num, calculate how much we'll remove.
 #If the numCount is less than num, we'll remove all of it.
 for num in ListA:
-    if num != -1:
-        numCount = ListA.count(num)
-        #print(f'{num} = {numCount} times.')
+    if num not in alreadyChecked:
 
-        ListA = [-1 if x == num else x for x in ListA]
-        #print(ListA)
+        numCount = ListA.count(num)
+        alreadyChecked.append(num)
 
         if num < numCount:
             removalCounter += (numCount - num)
         elif num > numCount:
             removalCounter += numCount
 
+
 #Printing the final removal count.
 print(removalCounter)
+"""
 
+# TLE TLE TLE TLE TLE TLE TLE TLE TLE TLE TLE TLE TLE TLE TLE TLE TLE TLE TLE TLE TLE TLE TLE TLE TLE TLE TLE TLE TLE
 
-
-
-#-----------------------------------------------------------------------------------------------------------------------
 """
 #Taking the input N as the size of the list. And then taking the list input.
 N = int(input())
@@ -61,3 +67,5 @@ for num in ListA:
 #Printing the final removal count.
 print(removalCounter)
 """
+
+# TLE TLE TLE TLE TLE TLE TLE TLE TLE TLE TLE TLE TLE TLE TLE TLE TLE TLE TLE TLE TLE TLE TLE TLE TLE TLE TLE TLE TLE
