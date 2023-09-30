e7db3001d764cada3bfc7919a93d23fe786d8aff
MZahidKamal <kamal.md.zahid@gmail.com>
2023-10-01 00:07:40 +0200

    Daily Learning & Commit

--- Week_01/Module_05/Video5.1.py
+++ Week_01/Module_05/Video5.1.py
@@ -1,3 +1,30 @@
 #-----------------------------------------------------------------------------------------------------------------------
 # 5.1 Introduction to Simple Class
 #-----------------------------------------------------------------------------------------------------------------------
+#The class creates a user-defined data structure, which holds its own data members and member functions,
+#which can be accessed and used by creating an instance of that class. A class is like a blueprint for an object.
+
+
+#Creating a user defined class. And initializing some values in it.
+class Phone:
+    Brand = 'Samsung'
+    Model = 'A71'
+    ReleaseYear = 2022
+    Price = 350
+
+#Creating an object of that class.
+myPhone = Phone()
+
+#Printing the object.
+print(myPhone)
+
+#If we write [objectName.] then the attributes will show up.
+#Printing the object attributes separately.
+print(myPhone.Brand)
+print(myPhone.Model)
+print(myPhone.ReleaseYear)
+print(myPhone.Price)
+
+
+# To Learn more, visit (https://www.w3schools.com/python/python_classes.asp).
+# To Learn more, visit (https://www.geeksforgeeks.org/python-classes-and-objects/).
--- Week_01/Module_05/Video5.2.py
+++ Week_01/Module_05/Video5.2.py
@@ -1,3 +1,82 @@
 #-----------------------------------------------------------------------------------------------------------------------
 # 5.2 Creating and Using Methods
 #-----------------------------------------------------------------------------------------------------------------------
+
+#Simple function can be written in this way.
+def call():
+    print('Calling Someone Global')
+    return 'Call done Global.'
+#But if we write the same code in the class, it'll work as method of the class.
+
+#-----------------------------------------------------------------------------------------------------------------------
+
+#Creating a user defined class. And initializing some values in it.
+class Phone:
+    Brand = 'Samsung'
+    Model = 'A71'
+    ReleaseYear = 2022
+    Price = 350
+    Features = ['Call', 'Message', 'Camera', 'Browsing', 'Calculator']
+
+    #Creating a method under the class.
+    def call(self, phone_number):
+        status = f'Calling {phone_number} to Someone from My Phone.'
+        return status
+
+    #Creating another method under the class.
+    def message(self, phone_number, text):
+        status = f'Texting "{text}" to {phone_number} to Someone from My Phone.'
+        return status
+
+
+#Creating an object of that class. And printing the object.
+myPhone = Phone()
+
+#If we write [objectName.] then the attributes will show up. Let's print the object attributes together.
+print(myPhone.Brand, myPhone.Model, myPhone.ReleaseYear, myPhone.Price, myPhone.Features)
+
+#If we write [objectName.] then beside the attributes, the [method] will also show up. Let's call the methods.
+callStatus = myPhone.call(+49123456789)
+print(callStatus)
+
+messageStatus = myPhone.message(+49123456789, 'Dont be late, meeting will start soon.')
+print(messageStatus)
+print()
+print()
+
+#-----------------------------------------------------------------------------------------------------------------------
+
+#Creating another user defined class. And initializing some values in it.
+class Calculator:
+    Brand = 'Casio MS990'
+    Features = ['Add', 'Subtract', 'Multiply', 'Division']
+
+    #Creating some methods under the class.
+    def add(self, num1, num2):
+        return f'Sum of {num1} and {num2} = {num1+num2}'
+
+    def subtract(self, num1, num2):
+        return f'Subtract of {num1} and {num2} = {num1 - num2}'
+
+    def multiply(self, num1, num2):
+        return f'Multiple of {num1} and {num2} = {num1 * num2}'
+
+    def divide(self, num1, num2):
+        return f'Division of {num1} and {num2} = {num1 / num2}'
+
+
+#Creating an object of that class. And printing the object.
+myCalculator = Calculator()
+print(myCalculator.Brand, myCalculator.Features)
+
+addStatus = myCalculator.add(22, 33)
+subtractStatus = myCalculator.subtract(22, 33)
+multiplyStatus = myCalculator.multiply(22, 33)
+divideStatus = myCalculator.divide(22, 33)
+
+print(addStatus)
+print(subtractStatus)
+print(multiplyStatus)
+print(divideStatus)
+
+#-----------------------------------------------------------------------------------------------------------------------
