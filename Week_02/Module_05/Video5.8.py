8f6d8553d70714d3678c5dca8364091100e641b2
MZahidKamal <kamal.md.zahid@gmail.com>
2023-10-02 00:02:13 +0200

    Daily Learning & Commit

--- Week_02/Module_05/Video5.8.py
+++ Week_02/Module_05/Video5.8.py
@@ -1,3 +1,6 @@
 #-----------------------------------------------------------------------------------------------------------------------
 # 5.8 Module summary and documentation
 #-----------------------------------------------------------------------------------------------------------------------
+# To Learn more, visit (https://docs.python.org/3/tutorial/classes.html).
+# To Learn more, visit (https://www.w3schools.com/python/python_classes.asp).
+# To Learn more, visit (https://www.geeksforgeeks.org/python-classes-and-objects/).
