a4c9f6571153e4e16440120b48704eb30d1e8291
MZahidKamal <kamal.md.zahid@gmail.com>
2023-10-02 14:49:17 +0200

    Daily Learning & Commit

--- Week_02/Module_06/Video6.4.py
+++ Week_02/Module_06/Video6.4.py
@@ -1,3 +1,31 @@
 #-----------------------------------------------------------------------------------------------------------------------
 # 6.4 Multiple Inheritance and Inheritance concepts
 #-----------------------------------------------------------------------------------------------------------------------
+"""
+Single Inheritance:
+Single inheritance enables a derived class to inherit properties from a single parent class, thus enabling code
+re-usability and the addition of new features to existing code.
+
+
+Multiple Inheritance:
+When a class can be derived from more than one base class this type of inheritance is called multiple inheritances.
+In multiple inheritances, all the features of the base classes are inherited into the derived class.
+
+
+Multilevel Inheritance :
+In multilevel inheritance, features of the base class and the derived class are further inherited into the new derived
+class. This is similar to a relationship representing a child and a grandfather.
+
+
+Hierarchical Inheritance:
+When more than one derived class are created from a single base this type of inheritance is called hierarchical
+inheritance. In this program, we have a parent (base) class and two child (derived) classes.
+
+
+Hybrid Inheritance:
+Inheritance consisting of multiple types of inheritance is called hybrid inheritance.
+
+
+#To Learn more, visit (https://www.geeksforgeeks.org/types-of-inheritance-python/).
+"""
+#-----------------------------------------------------------------------------------------------------------------------
