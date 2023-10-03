7d35baaddd830f865f7be48bf28fcbd5e8d005be
MZahidKamal <kamal.md.zahid@gmail.com>
2023-10-03 12:08:12 +0200

    Daily Learning & Commit

--- Week_02/Module_06/Video6.9.py
+++ Week_02/Module_06/Video6.9.py
@@ -1,3 +1,28 @@
 #-----------------------------------------------------------------------------------------------------------------------
 # 6.9 Module summary
 #-----------------------------------------------------------------------------------------------------------------------
+class Book:
+    def __init__(self, name):
+        self.name = name
+
+    def read(self):
+        #pass
+        raise NotImplementedError
+
+class Physics(Book):
+    def __init__(self, name, lab):
+        self.lab = lab
+        super().__init__(name)
+
+    def read(self):
+        print('Reading physics book.')
+
+topon = Physics('Topon Sir', True)
+
+print(issubclass(Physics, Book))
+print(isinstance(topon, Physics))
+print(isinstance(topon, Book))
+
+topon.read()
+
+#-----------------------------------------------------------------------------------------------------------------------
