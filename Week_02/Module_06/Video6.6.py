2baa9b93240794311ca6e1a55fab8f0ff6cbb586
MZahidKamal <kamal.md.zahid@gmail.com>
2023-10-03 03:02:33 +0200

    Daily Learning & Commit

--- Week_02/Module_06/Video6.6.py
+++ Week_02/Module_06/Video6.6.py
@@ -1,3 +1,33 @@
 #-----------------------------------------------------------------------------------------------------------------------
 # 6.6 Abstract Classes and abstractmethod
 #-----------------------------------------------------------------------------------------------------------------------
+from abc import ABC, abstractmethod
+
+class Animal(ABC):
+    @abstractmethod
+    def eat(self):
+        print('I need food.')
+
+    @abstractmethod
+    def move(self):
+        print('I want to play.')
+
+class Monkey(Animal):
+    def __init__(self, name):
+        self.category = 'Monkey'
+        self.name = name
+        super().__init__()
+
+    def eat(self):
+        print('Eating banana.')
+
+    def move(self):
+        print('Hanging on the branches.')
+
+
+lyca = Monkey('Lyca')
+lyca.eat()
+
+#-----------------------------------------------------------------------------------------------------------------------
+# To Learn more, visit (https://www.geeksforgeeks.org/abstract-classes-in-python/).
+#-----------------------------------------------------------------------------------------------------------------------
