346fd889f106f065381d4b2b3225c4c83ee4497f
MZahidKamal <kamal.md.zahid@gmail.com>
2023-10-04 19:51:48 +0200

    Daily Learning & Commit

--- Week_02/Module_07/Video7.2.py
+++ Week_02/Module_07/Video7.2.py
@@ -9,19 +9,38 @@ class Shopping:
         self.name = name                            # This is instance attribute
         self.location = location
 
+    @classmethod
     def purchase(self, item, price, amount):
         remaining = amount - price
         print(f'Buying {item} for {price} Euro, with remaining {remaining} Euro.')
 
+    @staticmethod
+    def multiple(a, b):
+        print(a*b)
+
+
 supermarket = Shopping('Rewe Supermarkt', '60439 Heddernheim')
 supermarket.purchase('A4 Paper', 5, 10)
-purchase('A4 Paper', 5, 10)
+Shopping.purchase('A4 Paper', 5, 10)
+
+Shopping.multiple(4, 6)
+supermarket.multiple(4, 6)
 
 """
 See the purchase method, and supermarket object. The purchase method is only accessible if we create an object and then
 call the method using the object, like we did here. This type of method is static method.
 
-But if we want to access the method without creating any class, like...
-purchase('A4 Paper', 5, 10)
+But if we want to access the method, from the class level, without creating any object, like...
+Shopping.purchase('A4 Paper', 5, 10)
 it will show error.
+
+But if we add [@classmethod] just above the method snippet, then this method will also be accessible from the class
+level. It will not show any error. And must not, but in that case, instead of self, cls can be used.
+
+Let's see another method [multiple]. If we write [@staticmethod], just above the snippet, then this method will also
+be accessible from the class level and as well as from the instance level. And we must not use the self in this method.
+
+#To Learn more, visit (https://www.geeksforgeeks.org/class-method-vs-static-method-python/).
+#To Learn more, visit (https://www.tutorialspoint.com/class-method-vs-static-method-in-python).
 """
+#-----------------------------------------------------------------------------------------------------------------------
--- Week_02/Module_07/Video7.3.py
+++ Week_02/Module_07/Video7.3.py
@@ -1,3 +1,56 @@
 #-----------------------------------------------------------------------------------------------------------------------
 # 7.3 Getter Setter and Read only property Using Property Decorator
 #-----------------------------------------------------------------------------------------------------------------------
+class User:
+    def __init__(self, name, age, money):
+        self._name = name
+        self._age = age
+        self.__money = money
+
+    @property                                   # Method converted to Property. Getter without setter. Read only mode.
+    def age(self):
+        return self._age
+
+    @property                                   # Method converted to Property. Getter without setter. Read only mode.
+    def money(self):
+        return self.__money
+
+    @money.setter                               # Setter for the money getter. Read only mode.
+    def money(self, new_value):
+        if new_value > 0:
+            self.__money += new_value
+        else:
+            print('Salary can\'t be 0.')
+
+
+
+jems = User('Jems', 23, 12000)
+
+#print(jems.name)       # No accessible, because protected. But with _name possible to access.
+#print(jems.age)        # No accessible, because protected. But with _age possible to access.
+#print(jems.money)      # No accessible, because private. Even with __money not possible to access.
+
+#If we create a method for age, then it will be accessible.
+#print(jems.age())
+#But see, there is () after age. That means we are using the age as a method.
+
+#Now if we add [@property] decorator just above the age method snippet, then the method age() will function like a
+#property of the class. And it will be accessible even without ().
+print(jems.age)
+print(jems.money)
+
+#Remember, this age and money were properties, but protected and private. And was not easily accessible.
+#Then we created methods for them and created a way to access them.
+#Then again we converted them back to properties, and they are now accessible. This process is actually called [getter].
+#Using getter, we may have access, but only in [Read only] mode. That means, we can read them, but can't write them.
+
+jems.money = 500
+#Without the setter, this line should give us error.
+#So we need setter to write them, or update the value of them.
+#Geater to get them and setter to set them, easy to understand.
+
+#After creating the setter, if we try to update the money with new value, it will not show error.
+#Rather the balance will be updated. Let's see the balance.
+print(jems.money)
+
+#-----------------------------------------------------------------------------------------------------------------------
