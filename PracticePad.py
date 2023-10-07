1e20e992e9eab06f1ff9ebf31922f9f6a4a08c1d
MZahidKamal <kamal.md.zahid@gmail.com>
2023-10-08 01:19:58 +0200

    Daily Learning & Commit

--- PracticePad.py
+++ PracticePad.py
@@ -43,7 +43,7 @@ class Counter(StarCinema):
             if (desired_row <= 0 or desired_row > rows) or (desired_column <= 0 or desired_column > columns):
                 print(f'Seat at row {desired_row} column {desired_column} is not existed. Please choose a valid seat.')
 
-            elif self.seats[desired_show_id][desired_row][desired_column] != '_Free_':
+            elif self.seats[desired_show_id][desired_row][desired_column] != ' Free ':
                 print(f'Seat at row {desired_row} column {desired_column} is already booked. Please choose another seat.')
 
             else:
@@ -66,27 +66,33 @@ class Hall(StarCinema):
         self.entry_hall(self)
 
 
-    def entry_show(self, show_id, show_name, show_time):
+    def entry_show(self, show_id, show_name, show_time, hall_no):
         #Creating a Tupple with show information, naming it 'show' and appending it into the show_list.
-        show = (show_id, show_name, show_time)
+        show = (show_id, show_name, show_time, hall_no)
         self.show_list.append(show)
         StarCinema.show_list.append(show)
         #
         #Creating a 2D matrix of rows and columns, and initializing all cells of the 2D matrix with 'Free'.
         #Naming it 'seat_matrix', putting it into the seats dictionary as KEY = show_id and VALUE = seat_matrix.
-        seat_matrix = [['_Free_' for _ in range(self.columns)] for _ in range(self.rows)]
+        seat_matrix = [[' Free ' for _ in range(self.columns)] for _ in range(self.rows)]
         self.seats[show_id] = seat_matrix
         StarCinema.seats[show_id] = seat_matrix
 
 
     def book_seat(self, desired_show_id, desired_row, desired_column):
-        if desired_show_id not in self.seats:
+        show_exist = False
+        for show in self.show_list:
+            if show[0] == desired_show_id:
+                show_exist = True
+                break
+
+        if not show_exist:
             print(f'Show ID {desired_show_id} is invalid. Provide a valid show ID, please.')
         else:
             if (desired_row <= 0 or desired_row > self.rows) or (desired_column <= 0 or desired_column > self.columns):
                 print(f'Seat at row {desired_row} column {desired_column} is not existed. Please choose a valid seat.')
 
-            elif self.seats[desired_show_id][desired_row][desired_column] != '_Free_':
+            elif self.seats[desired_show_id][desired_row][desired_column] != ' Free ':
                 print(f'Seat at row {desired_row} column {desired_column} is already booked. Please choose another seat.')
 
             else:
@@ -109,11 +115,34 @@ class Hall(StarCinema):
 
 
 #-----------------------------------------------------------------------------------------------------------------------
+#-----------------------------------------------------------------------------------------------------------------------
+#-----------------------------------------------------------------------------------------------------------------------
+
+#Adding some hard coded halls by default, including some shows in each hall.
+hall_101 = Hall(12, 20, 101)
+hall_101.entry_show('1972', 'The Godfather', '11:00', 101)
+hall_101.entry_show('1939', 'Gone with the Wind', '14:00', 101)
+
+
+hall_102 = Hall(12, 20, 102)
+hall_102.entry_show('1942', 'Casablanca', '12:00', 102)
+hall_102.entry_show('1994', 'Pulp Fiction', '15:00', 102)
+
+
+hall_103 = Hall(12, 20, 103)
+hall_103.entry_show('1993', 'Schindler\'s List', '13:00', 103)
+hall_103.entry_show('1994', 'The Shawshank Redemption', '16:00', 103)
+
+
+#-----------------------------------------------------------------------------------------------------------------------
+#-----------------------------------------------------------------------------------------------------------------------
+#-----------------------------------------------------------------------------------------------------------------------
+
 print()
 print('Hello. Welcome to Star Cinema Complex.')
 while True:
     print()
-    print('Choose your roll:')
+    print('----- Choose your roll: -----')
     print('', '1. Business Owner/Administrator', '\n', '2. Hall Supervisor', '\n', '3. Counter Manager', '\n', '4. Logout')
     print()
     userInput1 = int(input('Your selection: '))
@@ -159,34 +188,55 @@ while True:
         if totalHall == 0:
             print('No hall exists. Please get administrative privileges and add a hall.')
         else:
-            print('Currently available hall list:')
+            print()
+            print('----- Currently available hall list: -----')
             for hall in StarCinema.hall_list:
-                print(f'Hall No: {hall.hall_no}')
-            chosenHall = int(input('Choose a hall to get the access: '))
+                print(f'Hall ID: {hall.hall_no}: Total available seats: {hall.rows * hall.columns}.')
+            chosenHallNumber = int(input('Choose a hall to get the access: '))
 
-            hall_exists = any(hall.hall_no == chosenHall for hall in StarCinema.hall_list)
+            hall_exists = False
+            chosenHallObject = None
+            any(hall.hall_no == chosenHallNumber for hall in StarCinema.hall_list)
+            for hall in StarCinema.hall_list:
+                if hall.hall_no == chosenHallNumber:
+                    hall_exists = True
+                    chosenHallObject = hall
+
             if hall_exists is False:
-                print(f'Hall number {chosenHall} not exists.')
+                print(f'Hall number {chosenHallNumber} not exists.')
                 continue
             else:
                 while True:
                     print()
-                    print('Choose an option:')
-                    print('', '1. Complete show list with schedule', '\n', '2. Enter a new show to a hall', '\n', '3. Available seats of a show', '\n', '4. Book a seat for a show', '\n', '5. Logout')
+                    print('----- Choose an option: -----')
+                    print('', '1. Complete show list of this hall', '\n', '2. Enter a new show to this hall', '\n', '3. Available seats of a show', '\n', '4. Book a seat for a show', '\n', '5. Logout')
                     print()
                     userInput3 = int(input('Your selection: '))
 
                     if userInput3 == 1:
-                        print('Complete show list with schedule')
+                        print('Complete show list of this hall')
+                        for show in StarCinema.show_list:
+                            if show[3] == chosenHallNumber:
+                                print(f'ID: {show[0]},    Name: {show[1]},      Start Time: {show[2]}')
 
                     elif userInput3 == 2:
-                        print('Enter a new show to a hall')
+                        show_id = int(input('Provide show ID: '))
+                        show_name = input('Provide show Name: ')
+                        show_time = input('Provide show Time: ')
+                        chosenHallObject.entry_show(show_id, show_name, show_time, chosenHallNumber)
+                        print(
+                            f'Show "{show_name}" with ID {show_id} added to Hall {chosenHallNumber} at {show_time} o\'clock.')
 
                     elif userInput3 == 3:
-                        print('Available seats of a show')
+                        show_id = input('Provide show ID: ')
+                        print()
+                        chosenHallObject.view_available_seats(show_id)
 
                     elif userInput3 == 4:
-                        print('Book a seat for a show')
+                        show_id = int(input('Provide show ID: '))
+                        desired_row = input('Row of your preferred seat: ')
+                        desired_column = input('Column of your preferred seat: ')
+                        chosenHallObject.book_seat(show_id, desired_row, desired_column)
 
                     elif userInput3 == 5:
                         print('Logged out from Hall Supervisory privileges.')
